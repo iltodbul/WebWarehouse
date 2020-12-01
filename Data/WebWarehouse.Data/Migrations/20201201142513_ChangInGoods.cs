e8c797624b077282455acd6a5cb06842db6b6f73
Iliya Todorov <53480117+iltodbul@users.noreply.github.com>
2020-12-01 16:26:47 +0200

    Add migration

---
+++ Data/WebWarehouse.Data/Migrations/20201201142513_ChangInGoods.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebWarehouse.Data.Migrations
+{
+    public partial class ChangInGoods : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Discount",
+                table: "Goods");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Discount",
+                table: "Goods",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+    }
+}
